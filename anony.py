883f4fe7430077fd2aed7ddfa59124adc28d0a2b
jrussell <jrussell@doorfunds.com>
2020-03-26 22:10:04 +0000

    Include SQL

--- anony.py
+++ anony.py
@@ -6,30 +6,31 @@ from crumbs.Introduction import INTRODUCTION, display_database_information
 from crumbs.query_controller import truncate_query, executable_query
 from crumbs.anonymizing import modify
 
+
 init(autoreset=True)
 
 start_time = time()
 
+
 with open(r"config/settings.json", "r") as f:
     configuration = load(f)
+    RDB = configuration["database"]["src"]
 
 print(INTRODUCTION)
 display_database_information(configuration)
-
-# Small little show the database config settings
 sleep(2)
 
 list_of_functions = [
-    lambda: truncate_query(configuration["truncate"]),
-    lambda: executable_query("Custom Queries", configuration["custom_queries"]),
-    lambda: modify(configuration["table_data"]),
-    lambda: executable_query("Final Queries", configuration["final_queries"]),
+    lambda: truncate_query(RDB, configuration["truncate"]),
+    lambda: executable_query(RDB, "Custom Queries", configuration["custom_queries"]),
+    lambda: modify(RDB, configuration["table_data"]),
+    lambda: executable_query(RDB, "Final Queries", configuration["final_queries"]),
 ]
-
+#
 for function in list_of_functions:
     try:
         function()
     except KeyError as e:
         print(f"{Fore.YELLOW}[WARN] {Fore.RESET}{e} not declared.")
-
+#
 print(f"Finished! Completion Time:  ({round(time() - start_time, 2)} seconds)")
--- crumbs/query_controller.py
+++ crumbs/query_controller.py
@@ -7,19 +7,25 @@ init(autoreset=True)
 sql = DatabaseBuilder()
 
 
-def truncate_query(tables):
+def truncate_query(db, tables):
     tprint("Truncating Tables", font="cybermedium")
     for table in tables:
-        sql.execute_query_with_disabled_sql_check(f"Truncate {table}")
+        if db == "mysql":
+            sql.execute_query_with_disabled_sql_check(f"Truncate {table}")
+        if db == "sqlserver":
+            sql.sqlServerTruncateQuery(f"Truncate Table {table}")
         print(
             f"{Fore.YELLOW}[TABLE] - {Fore.RESET}{Fore.CYAN}{table}{Fore.RESET} ::{Fore.GREEN} Truncated successfully."
         )
 
 
-def executable_query(title, queries):
+def executable_query(db, title, queries):
     tprint(title, font="cybermedium")
     for inx, query in enumerate(queries):
         print(
             f'{Fore.GREEN}[{inx + 1}/{len(queries)}]{Fore.RESET} :: "{Fore.YELLOW}{query};{Fore.RESET}" '
         )
-        sql.execute_query_with_disabled_sql_check(query)
+        if db == "mysql":
+            sql.execute_query_with_disabled_sql_check(query)
+        if db == "sqlserver":
+            sql.sqlServerTruncateQuery(query)
